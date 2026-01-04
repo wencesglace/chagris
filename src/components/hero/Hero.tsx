7f301766d2010b69a5c482d048673a7d3a7755de
wencesglace <wenceslace@gmail.com>
2026-01-04 14:36:41 +0100

    feat: improving UI

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,16 +8,12 @@ export default function Hero({ className }: HeroProps) {
   return (
     <section className={`hero ${className}`}>
       <div className="content layout-half">
-        <p className="h3">
-          Nous sommes une association loi 1901 de protection et de nourrissage
-          des chats des rues.
+        <p className="body1">
+          Nous sommes une association loi 1901 de protection
+          <br />
+          et de nourrissage des chats des rues.
         </p>
-        <img
-          src="src/assets/logo/logo-white.png"
-          alt="Logo"
-          className="logo"
-        />
-        
+        <img src="src/assets/logo/logo-white.png" alt="Logo" className="logo" />
       </div>
       <img
         src="src/components/hero/chartreux.png"
